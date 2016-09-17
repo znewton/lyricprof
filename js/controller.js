abd14e1e2c34969d8a1531ce38d187d2183c08d2
Figgynewts <znewton@iastate.edu>
2016-09-16 22:09:25 -0500

    Realizing I can't scrape from client side

--- js/controller.js
+++ js/controller.js
@@ -1,8 +1,19 @@
-app.controller('ctrl', ['$scope', function($scope) {
-    $scope.search_song = '';
-    $scope.search_artist = '';
+app.controller('ctrl', ['$scope', '$http', function($scope, $http) {
+    $scope.search_song = 'closer';
+    $scope.search_artist = 'The_Chainsmokers';
+    $scope.resultTest = '';
+
+    const wikiaSearchURL = 'http://lyrics.wikia.com/wiki/';
 
     $scope.search = function() {
+        searchURL = wikiaSearchURL + $scope.search_artist + ':' + $scope.search_song;
 
+        $http.get(searchURL, []).then(function(response) {
+            $scope.resultTest = 'Success';
+            console.log(response);
+        }, function(response) {
+            $scope.resultTest = 'Failure';
+            console.log(response);
+        })
     }
 }]);
