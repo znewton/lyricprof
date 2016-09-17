6e4e83d2b3814953b3daaa0f8486ca6e7ab9768c
Figgynewts <znewton@iastate.edu>
2016-09-17 09:10:05 -0500

    Apparently APIs don't allow client side calls either :(

--- js/controller.js
+++ js/controller.js
@@ -2,18 +2,38 @@ app.controller('ctrl', ['$scope', '$http', function($scope, $http) {
     $scope.search_song = '';
     $scope.search_artist = '';
     $scope.resultTest = '';
+    $scope.song_found = false;
+    $scope.song_unfound = false;
 
-    const wikiaSearchURL = 'http://lyrics.wikia.com/wiki/';
+    const apiUrl = 'http://api.musixmatch.com/ws/1.1/';
+    const trackSearchUrl = 'track.search';
+    const testURL = 'http://lyrics.wikia.com/api.php?func=getSong&artist=' + $scope.search_artist + '&song=' + $scope.search_song+'&fmt=xml';
 
     $scope.search = function() {
-        searchURL = wikiaSearchURL + $scope.search_artist + ':' + $scope.search_song;
+        $scope.song_found = false;
+        $scope.song_unfound = false;
 
-        $http.get(searchURL, []).then(function(response) {
+        //https://developer.musixmatch.com/documentation/input-parameters
+        var postObj = {
+            apikey: 'b4d35084f95ba7ab1c29f553523fe610',
+            q_track: $scope.search_song,
+            q_artist: $scope.search_artist,
+            f_has_lyrics: true,
+            s_artist_rating: 'DESC'
+        };
+//apiUrl + trackSearchUrl
+        $http.get(testURL, postObj).then(function(response) {
             $scope.resultTest = 'Success';
+            $scope.song_found = true;
+            $scope.song_unfound = false;
             console.log(response);
+
         }, function(response) {
             $scope.resultTest = 'Failure';
+            $scope.song_unfound = true;
+            $scope.song_found = false;
             console.log(response);
+
         })
     }
 }]);
