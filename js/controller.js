ecb2ff36ad74a65fbe68953f6c604f757ac7017c
Figgynewts <znewton@iastate.edu>
2016-09-22 09:19:37 -0500

    Trying to add search scrape functionality. Results aren't as expected

--- js/controller.js
+++ js/controller.js
@@ -41,7 +41,8 @@ app.controller('ctrl', ['$scope', '$http', function($scope, $http) {
 //             $scope.song_unfound = true;
 //             console.log(data);
 //         });
-        if($scope.search_song != '' && $scope.search_artist != '') {
+        //&& $scope.search_artist != ''
+        if($scope.search_song != '' ) {
             $scope.searching = true;
             $http.post(testURL, postObj, []).then(function (response) {
                 $scope.searching=false;
@@ -51,24 +52,15 @@ app.controller('ctrl', ['$scope', '$http', function($scope, $http) {
                 $scope.songTitle = response.data.final_song;
                 $scope.artistTitle = response.data.final_artist;
 
+                $scope.searchResult = response.data.search_result;
+
                 $scope.search_song = response.data.final_song;
                 $scope.search_artist = response.data.final_artist;
-                if ($scope.flaggedLyrics && $scope.flaggedLyrics.length > 0) {
-                    $scope.dirty = true;
-                    if($scope.flaggedLyrics.length > 40){
-                        $scope.profanityProbability = 'Almost Certainly';
-                    } else if($scope.flaggedLyrics.length > 20){
-                        $scope.profanityProbability = 'Most Likely';
-                    } else if($scope.flaggedLyrics.length > 10){
-                        $scope.profanityProbability = 'Probably';
-                    } else if($scope.flaggedLyrics.length > 1){
-                        $scope.profanityProbability = 'Possibly';
-                    } else {
-                        $scope.profanityProbability = 'Maybe';
-                    }
-                }
+
                 if ($scope.fullLyrics && $scope.fullLyrics.length > 1){
                     success();
+                } else if($scope.searchResult.length > 0){
+                    searchSuccess();
                 } else {
                     fail();
                 }
@@ -83,14 +75,56 @@ app.controller('ctrl', ['$scope', '$http', function($scope, $http) {
 
     function success(){
         console.log('success');
+        if ($scope.flaggedLyrics && $scope.flaggedLyrics.length > 0) {
+            $scope.dirty = true;
+            if($scope.flaggedLyrics.length > 40){
+                $scope.profanityProbability = 'Almost Certainly';
+            } else if($scope.flaggedLyrics.length > 20){
+                $scope.profanityProbability = 'Most Likely';
+            } else if($scope.flaggedLyrics.length > 10){
+                $scope.profanityProbability = 'Probably';
+            } else if($scope.flaggedLyrics.length > 1){
+                $scope.profanityProbability = 'Possibly';
+            } else {
+                $scope.profanityProbability = 'Maybe';
+            }
+        }
         $scope.song_found = true;
         $scope.song_unfound = false;
+    }
+    function searchSuccess() {
+        console.log('Search Success');
+        $scope.searchLinkList = [];
+        var link = '';
+        var artist = '';
+        var song = '';
+        var colonIndex = 0;
+        for(var i = 0; i < $scope.searchResult.length; i++){
+            // console.log()
+            link = $scope.searchResult[i]['attr'].href;
+            link = link.replace('http://lyrics.wikia.com/wiki/', '');
+            colonIndex = link.indexOf(':');
+            artist = decodeURIComponent(link.substring(0, colonIndex)).replace(/_/g, ' ');
+            song = decodeURIComponent(link.substr(colonIndex+1)).replace(/_/g, ' ');
+            $scope.searchLinkList.push({
+                artist: artist,
+                song: song
+            })
+        }
+        $scope.song_found = false;
+        $scope.song_unfound = true;
     }
     function fail(){
         console.log('fail');
         $scope.song_found = false;
         $scope.song_unfound = true;
     }
+
+    $scope.searchFromLink = function(song, artist){
+        $scope.search_song = song;
+        $scope.search_artist = artist;
+        $scope.search();
+    };
 
     $scope.toggleErrorCollapse = function() {
         $scope.errorCollapse = !$scope.errorCollapse;
--- searchSong.php
+++ searchSong.php
@@ -4,9 +4,12 @@ require_once('lib/naughtyWords.php');
 
 set_time_limit(0);// to infinity and beyond
 
-function myUrlEncode($string) {
+function myUrlEncode($string, $search = false) {
     $entities = array('_', '%26', '%27', '%3f');
     $replacements = array(' ', "&", "'", '?');
+    if($search){
+        $entities[0] = '+';
+    }
     return str_replace($replacements, $entities, $string);
 }
 function myUrlDecode($string) {
@@ -14,13 +17,31 @@ function myUrlDecode($string) {
     $replacements = array('_', '%26', '%27', '%3f');
     return str_replace($replacements, $entities, $string);
 }
+function unique_multidim_array($array, $key) {
+    $temp_array = array();
+    $i = 0;
+    $key_array = array();
+
+    foreach($array as $val) {
+        if (!in_array($val[$key], $key_array)) {
+            $key_array[$i] = $val[$key];
+            $temp_array[$i] = $val;
+        }
+        $i++;
+    }
+    return $temp_array;
+}
 
 $input = file_get_contents("php://input");
 $postdata = json_decode($input);
 $song = $postdata->song;
 $song = myUrlEncode($song);
-$artist = $postdata->artist;
-$artist = myUrlEncode($artist);
+if($postdata->artist) {
+    $artist = $postdata->artist;
+    $artist = myUrlEncode($artist);
+} else {
+    $artist = null;
+}
 
 /**
  * @param $attemptNum
@@ -33,15 +54,15 @@ $artist = myUrlEncode($artist);
 function alterSearchFormat($attemptNum, $attemptMultiplier,  $song, $artist) {
     //TODO: try camelcase song,
     //TODO: add a the to beginning of song, artist, both
-    if ($attemptNum > 3*$attemptMultiplier) {
+//    if ($attemptNum > 3*$attemptMultiplier) {
         //TODO: replace " and " with " & " in song and artist
-    }
-    elseif ($attemptNum > 2*$attemptMultiplier) {
+//    }
+//    elseif ($attemptNum > 2*$attemptMultiplier) {
         //TODO: replace " and " with " & " in song
-    }
-    elseif ($attemptNum > 1*$attemptMultiplier) {
+//    }
+//    elseif ($attemptNum > 1*$attemptMultiplier) {
         //TODO: replace " and "  with " & "  in artist
-    }
+//    }
 
     //default return statement. Unformatted search
      return [
@@ -74,42 +95,89 @@ error_reporting(E_ERROR);
 //$html = file_get_html($url);
 $attempts = 0;
 $lyricDiv = null;
-$songTitleDiv = null;
 $attemptMultiplier = 3;
 $numFormatChecks = 0; // number of additional formatting attempt checks to try. 0 is base format only
 
-while($attempts < ($attemptMultiplier*($numFormatChecks+1)) && !$lyricDiv){
+//Scrape Lyrics
+if($artist) {
+    while ($attempts < ($attemptMultiplier * ($numFormatChecks + 1)) && !$lyricDiv) {
 
-    $searchFormat = alterSearchFormat($attempts, $attemptMultiplier, $song, $artist);
-    $search_song = $searchFormat['song'];
-    $search_artist = $searchFormat['artist'];
-    $url="http://lyrics.wikia.com/wiki/" . $search_artist . ":" . $search_song;
+        $searchFormat = alterSearchFormat($attempts, $attemptMultiplier, $song, $artist);
+        $search_song = $searchFormat['song'];
+        $search_artist = $searchFormat['artist'];
+        $url = "http://lyrics.wikia.com/wiki/" . $search_artist . ":" . $search_song;
 
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-    curl_setopt($curl, CURLOPT_HEADER, false);
-    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_REFERER, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-    curl_setopt($curl, CURLOPT_CONNECTTIMEOUT , 5);
-    curl_setopt($curl, CURLOPT_TIMEOUT, 10); //timeout in seconds
-    $str = curl_exec($curl);
-    // Create a DOM object
-    $html = new simple_html_dom();
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_REFERER, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 10); //timeout in seconds
+        $str = curl_exec($curl);
+        // Create a DOM object
+        $html = new simple_html_dom();
 
-    // Load HTML from a string
-    $html->load($str);
-    if($html && $html->find('div[class=lyricbox]')) {
-        $lyricDiv = $html->find('div[class=lyricbox]')[0]->plaintext;
+        // Load HTML from a string
+        $html->load($str);
+        if ($html && $html->find('div[class=lyricbox]')) {
+            $lyricDiv = $html->find('div[class=lyricbox]')[0]->plaintext;
+        }
+        $html->clear();
+        unset($html);
+        curl_close($curl);
+        $attempts++;
     }
-    $html->clear();
-    unset($html);
-    curl_close($curl);
-    $attempts++;
 }
 
+$attempts = 0;
+$searchDiv = null;
+$attemptMultiplier = 3;
+//Scrape Search
+if(!$artist || !$lyricDiv){
+    while ($attempts < $attemptMultiplier && !$searchDiv) {
 
+        $search_song = myUrlEncode($postdata->song, true);
+        if($postdata->artist){
+            $search_artist = myUrlEncode($postdata->song, true);
+        } else {
+            $search_artist = '';
+        }
+        $url = "http://lyrics.wikia.com/wiki/Special:Search?search=" . $search_song . "+" . $search_artist . '&fulltext=Search&page=1&ns0=1';
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_REFERER, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 10); //timeout in seconds
+        $str = curl_exec($curl);
+        // Create a DOM object
+        $html = new simple_html_dom();
+
+        // Load HTML from a string
+        $html->load($str);
+        $searchDiv = [];
+        if ($html && $html->find('a[class=result-link]')) {
+            $list = $html->find('a[class=result-link]');
+            for($i = 0; $i < count($list); $i++){
+                $searchDiv[] = $list[$i];
+                $i++;
+            }
+//            $searchDiv = unique_multidim_array($searchDiv, 'attr');
+            $searchDiv = array_slice($searchDiv, 0, 20);
+        }
+        $html->clear();
+        unset($html);
+        curl_close($curl);
+        $attempts++;
+    }
+}
 
 $lyric= html_entity_decode($lyricDiv);
 $lyric = str_replace( '&#39;', "'", $lyric);
@@ -127,6 +195,7 @@ $data = [
     'flags' => flagLyrics($lyric_lines, naughtyWords),
     'final_song' => $finalSong,
     'final_artist' => $finalArtist,
+    'search_result' => $searchDiv,
 ];
 
 header('Content-Type:application/json');
